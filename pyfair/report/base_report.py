2215c32bb695dd5a24b6f5aa9c4b3834bbdab959
Dr. Michael Drass <92912773+DrMichaelDrass@users.noreply.github.com>
2024-01-03 05:35:25 +0100

    Update base_report.py

--- pyfair/report/base_report.py
+++ pyfair/report/base_report.py
@@ -29,7 +29,7 @@ class FairBaseReport(object):
     instantiated on its own.
 
     """
-    def __init__(self, currency_prefix='€'):
+    def __init__(self, currency_prefix='Euro'):
         # Add formatting strings
         self._currency_prefix = currency_prefix
         self._model_or_models = None
